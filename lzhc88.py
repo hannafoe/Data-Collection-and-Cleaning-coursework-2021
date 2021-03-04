306b198bd418390f8422009af8e86aa69bd4c874
hannafoe <hana.foerster@gmail.com>
2021-03-04 10:09:08 +0100

    push

--- lzhc88.py
+++ lzhc88.py
@@ -6,6 +6,10 @@ import textwrap
 import re
 import os
 ##python version 3.8
+keywords = ['targeted threat','Advanced Persistent Threat',
+'phishing','DoS attack','malware','computer virus','spyware',
+'malicious bot','ransomware','encryption']
+
 ## Problem 1
 #Python program that downloads the webpage content (from BBC news)
 #of the top 100 articles relevant to the given keywords from
@@ -24,9 +28,6 @@ except Exception as err:
     print(f'Other error occurred: {err}')
 else:
     print("success")
-keywords = ['targeted threat','Advanced Persistent Threat',
-'phishing','DoS attack','malware','computer virus','spyware',
-'malicious bot','ransomware','encryption']
 for key in keywords:
     link_list = []
     p=1
@@ -51,6 +52,18 @@ for key in keywords:
                 if 'programmes' in link and 'news' not in link:
                     continue
                 else:
+                    #There are websites with guide in the url that do not open
+                    #so check if urls with guide open
+                    if 'guides' in link: 
+                        try:
+                            res = requests.get(link,stream=True)
+                            res.raise_for_status()
+                        except HTTPError as http_err:
+                            print(f'HTTP error occurred: {http_err}')
+                            continue
+                        except Exception as err:
+                            print(f'Other error occurred: {err}')
+                            continue
                     #print(link)
                     link_list.append(link)
             p+=1
@@ -64,8 +77,8 @@ for element in link_dict:
     if len(link_dict[element])>=100:
         link_dict[element]=link_dict[element][:100]
 
-print(num_pages)
-print(num_articles)
+#print(num_pages)
+#print(num_articles)
 
 ## Problem 2
 #Use BeautifulSoup to collect and process the articles contents. 
@@ -110,8 +123,6 @@ for key in link_dict:
                     #text = text.select('p')
                     text = text.get_text()
                     #txt = text.encode(formatter="html")
-                    #text=text.prettify(formatter="html")##Fehleranzeige
-                    #print(text)
                     #txt = text.encode('utf-8','ignore').decode('utf-8')
                     #print(text)
                     #print(txt)
@@ -119,9 +130,50 @@ for key in link_dict:
                     #print(text)
             
     
-    
 
 ## Problem 3
+#Program to calculate the semantic distances between each two keywords
+#which belong to the list of keywords saved in keywords.xlsx
+#First use data downloaded from BBC news
+#
+#Repeated code in case part 1 and 2 are commented out
+num_articles=[0,0,0,0,0,0,0,0,0,0]
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+new_dir = "articles"
+path = os.path.join(cur_dir,new_dir)
+#Search for the other keywords in the articles
+corr_matrix = [[0,0,0,0,0,0,0,0,0,0] for i in range(len(keywords))]
+i=-1
+k=0
+for key in keywords:
+    i+=1
+    current = key
+    #print(current+"\n")
+    for file in os.listdir(path):
+        if file.startswith(key):
+            num_articles[i]+=1
+            path_file = os.path.join(path,file)
+            with open(path_file,'r',encoding='utf8',errors='ignore') as f:
+                j=-1
+                for other in keywords:
+                    j+=1
+                    f.seek(0)
+                    #print(key)
+                    if other!=current and other in f.read():
+                        #print(other+" in txt")
+                        corr_matrix[i][j]+=1
+print(corr_matrix)
+
+for i in range(len(corr_matrix)):
+    for j in range(len(corr_matrix)):
+        if i==j:
+            corr_matrix[i][j]=1
+        else:
+            if corr_matrix[i][j]!=0:
+                corr_matrix[i][j]=corr_matrix[i][j]/num_articles[i]
+print(num_articles)
+print(corr_matrix)
+
 
 ## Problem 4
 
