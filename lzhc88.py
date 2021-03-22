41de5d0264a13597d020512ea65989eacc65831b
hannafoe <hana.foerster@gmail.com>
2021-03-22 17:42:03 +0100

    push

--- lzhc88.py
+++ lzhc88.py
@@ -6,10 +6,73 @@ import textwrap
 import re
 import os
 ##python version 3.8
+#reusable functions and variables
 keywords = ['targeted threat','Advanced Persistent Threat',
 'phishing','DoS attack','malware','computer virus','spyware',
 'malicious bot','ransomware','encryption']
+check_keywords = [['target','threat'],['advance','persist','threat'],
+['phish']]
+def try_url(url):
+    try:
+        res = requests.get(url,stream=True,timeout=1)
+        # If the response was successful, no Exception will be raised
+        res.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+        return False
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+        return False
+    else:
+        print("success")
+        return True
 
+def check_if_word_in_website(url,word):
+    try:
+        res = requests.get(url,stream=True)
+        res.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        #Get the text component of the website
+        txt = res.text
+        soup = BeautifulSoup(txt, features='lxml')
+        if word in soup.title.get_text():
+            return True
+        #Extract only the article content
+        text_blocks = soup.find_all(attrs={"data-component":"text-block"})
+        for text in text_blocks:
+            text = text.get_text()
+            if word in text:
+                return True
+
+def write_urltext_into_file(url,path,i):
+    try:
+        res = requests.get(url,stream=True)
+        res.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        #Get the text component of the website
+        txt = res.text
+        soup = BeautifulSoup(txt, features='lxml')
+        #Extract only the article content
+        text_blocks = soup.find_all(attrs={"data-component":"text-block"})
+        #Create new text file to store the article
+        filename=key+" "+str(i)+".txt"
+        path_file = os.path.join(path,filename)
+        with open(path_file,'w',encoding='utf8',errors='ignore') as f:
+            #Write the title into the text file
+            f.write(soup.title.get_text()+"\n")
+            #Then write the whole textblock into the file
+            for text in text_blocks:
+                text = text.get_text()
+                f.write(text+"\n")
+'''
 ## Problem 1
 #Python program that downloads the webpage content (from BBC news)
 #of the top 100 articles relevant to the given keywords from
@@ -18,16 +81,7 @@ url = 'https://www.bbc.co.uk/search'
 link_dict ={}
 num_pages=[]
 num_articles=[]
-try:
-    res = requests.get(url,stream=True,timeout=0.3)
-    # If the response was successful, no Exception will be raised
-    res.raise_for_status()
-except HTTPError as http_err:
-    print(f'HTTP error occurred: {http_err}')
-except Exception as err:
-    print(f'Other error occurred: {err}')
-else:
-    print("success")
+
 for key in keywords:
     link_list = []
     p=1
@@ -48,24 +102,39 @@ for key in keywords:
             if not headlines:
                 break
             for headline in headlines:
+                print(headline)
                 link = headline['href']
                 if 'programmes' in link and 'news' not in link:
                     continue
                 else:
                     #There are websites with guide in the url that do not open
                     #so check if urls with guide open
-                    if 'guides' in link: 
-                        try:
-                            res = requests.get(link,stream=True)
-                            res.raise_for_status()
-                        except HTTPError as http_err:
-                            print(f'HTTP error occurred: {http_err}')
-                            continue
-                        except Exception as err:
-                            print(f'Other error occurred: {err}')
-                            continue
-                    #print(link)
-                    link_list.append(link)
+                    relevant_url = False
+                    good_url = True
+                    if 'guides' in link:
+                        print(link) 
+                        good_url = try_url(link)
+                        print(good_url)
+                    #Split keyword at space
+                    #Check if one of the components of keyword is included in the header
+                    #THIS METHOD TAKES AWAY ARTICLES THAT ARE IRRELEVANT; BUT ALSO SOME ARTICLES THAT ARE RELEVANT
+                    #SO DECIDED TO TAKE IT OUT
+                    #I FIND THE SEARCH ALGORITHM OF BBC MORE ACCURATE
+                    #key_split = key.split()
+                    #keyInHeader = headline.find_all(string=[re.compile(k, re.IGNORECASE) for k in key_split])
+                    #
+                    #if none of the keyword's component is in the header, check in the article itself
+                    #whether the article contains any of the keyword's components
+                    #THIS METHOD TAKES A LONG TIME AND IS NOT THAT HELPFUL: SO DECIDED TO TAKE IT OUT
+                    #if not keyInHeader:
+                    #    for word in key_split:
+                    #        if check_if_word_in_website(link,word) ==True:
+                    #            relevant_url=True
+                    #            break
+                    relevant_url=True
+                    if good_url==True and relevant_url==True:
+                        print(link)
+                        link_list.append(link)
             p+=1
     #print(p)
     #print()
@@ -94,43 +163,12 @@ try:
 except OSError as error:
     print("The directory 'articles' could not be created.")
 
+#Now create a text file for each article/url
 for key in link_dict:
     i=0
     for url in link_dict[key]:
         i+=1
-        try:
-            res = requests.get(url,stream=True)
-            res.raise_for_status()
-        except HTTPError as http_err:
-            print(f'HTTP error occurred: {http_err}')
-        except Exception as err:
-            print(f'Other error occurred: {err}')
-        else:
-            #Get the text component of the website
-            txt = res.text
-            soup = BeautifulSoup(txt, features='lxml')
-            #Get title
-            #print(soup.title.get_text())
-            #Extract only the article content
-            text_blocks = soup.find_all(attrs={"data-component":"text-block"})
-            #Create new text file to store the article
-            filename=key+" "+str(i)+".txt"
-            path_file = os.path.join(path,filename)
-            with open(path_file,'w',encoding='utf8',errors='ignore') as f:
-                #Write the title into the text file
-                f.write(soup.title.get_text()+"\n")
-                #Then write the whole textblock into the file
-                for text in text_blocks:
-                    #text = text.select('p')
-                    text = text.get_text()
-                    #txt = text.encode(formatter="html")
-                    #txt = text.encode('utf-8','ignore').decode('utf-8')
-                    #print(text)
-                    #print(txt)
-                    f.write(text+"\n")
-                    #print(text)
-            
-    
+        write_urltext_into_file(url,path,i)    
 
 ## Problem 3
 #Program to calculate the semantic distances between each two keywords
@@ -174,7 +212,33 @@ for i in range(len(corr_matrix)):
                 corr_matrix[i][j]=corr_matrix[i][j]/num_articles[i]
 print(num_articles)
 print(corr_matrix)
+'''
+#Work with google searches
+#Extract the related searches and look if one of the other keywords
+#is in the related searches
+url_google = 'https://www.google.com/search'
+url_wikipedia = 'https://en.wikipedia.org/wiki/'
+for key in keywords:
+    try:
+        r = requests.get(url_wikipedia+key)
+        r.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        print(r.url)
+        txt = r.text
+        soup = BeautifulSoup(txt, features='lxml')
+        for other_key in keywords:
+            if key == other_key:
+                continue
+            else:
+                key_in_wikipedia = check_if_word_in_website(link,other_key)
+                print(key_in_wikipedia)
 
+                
+    break        
 
 ## Problem 4
 
